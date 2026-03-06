3f8ec7da254d26a2cefedadbbb779a07d7a5ee72
Gastón Chapartegui <126299180+GChapartegui@users.noreply.github.com>
2026-03-06 13:36:01 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'futbol-elite-v1';
+const FILES = [
+  '/futbol-viernes/',
+  '/futbol-viernes/index.html',
+  '/futbol-viernes/manifest.json',
+  '/futbol-viernes/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
